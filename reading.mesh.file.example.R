d716c66d1b9cde0353d44160bb66c0ecbd562461
aldoclemente <62096496+aldoclemente@users.noreply.github.com>
2023-09-18 16:27:56 +0200

    read.mesh reads .mesh files (FreeFem++ output)

---
+++ reading.mesh.file.example.R
@@ -0,0 +1,27 @@
+rm(list=ls())
+source("read.mesh.R")
+
+# Square
+filename <-"data/square.mesh"
+domain <- read.mesh(filename)
+mesh <- create.mesh.2D(nodes=domain$nodes, triangles = domain$elements)
+plot(mesh)
+
+# Surface
+filename <-"data/sphere.surface.mesh"
+domain <- read.mesh(filename)
+mesh <- create.mesh.2.5D(nodes=domain$nodes, triangles = domain$elements)
+plot(mesh)
+
+# Cube
+filename <-"data/cube.mesh"
+domain <- read.mesh(filename)
+mesh <- create.mesh.3D(nodes=domain$nodes, tetrahedrons = domain$elements)
+plot(mesh)
+
+# Sphere3D
+filename <-"data/sphere3D.mesh"
+domain <- read.mesh(filename)
+mesh <- create.mesh.3D(nodes=domain$nodes, tetrahedrons = domain$elements)
+plot(mesh)
+
